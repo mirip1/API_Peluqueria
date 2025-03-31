14d5b79cc08130539ece9d59589629ac27d99d03
mirip1 <mivelfer5@gmail.com>
2025-03-31 13:55:43 +0200

    Coseguimos que funcione las llamadas api de Usuario correctamente habiendo implementado SpringSecurity y JWT (POR FIN)

--- src/main/java/com/fct/peluqueria/security/SecurityConfig.java
+++ src/main/java/com/fct/peluqueria/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.fct.peluqueria.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,43 +14,40 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-               
-                .requestMatchers("/api/usuarios/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .usernameParameter("email") 
-                .defaultSuccessUrl("/api/usuarios/profile", true)
-            )
-            .httpBasic(Customizer.withDefaults()); 
 
-        return http.build();
-    }
-    
-    @Bean
-    public UserDetailsService testUser(PasswordEncoder passwordEncoder) {
-      
-      User.UserBuilder user = User.builder();
-      UserDetails user1 = user.username("tfg")
-          .password(passwordEncoder.encode("hola"))
-          .build();
-      return new InMemoryUserDetailsManager(user1);
-      
-    }
-    
-    
+  @Autowired
+  @Lazy
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
+
+        .requestMatchers("/api/usuarios/**").permitAll().anyRequest().authenticated())
+        .formLogin(form -> form.usernameParameter("email").defaultSuccessUrl("/api/usuarios/profile", true))
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+//  @Bean
+//  public UserDetailsService testUser(PasswordEncoder passwordEncoder) {
+//
+//    User.UserBuilder user = User.builder();
+//    UserDetails user1 = user.username("tfg").password(passwordEncoder.encode("hola")).build();
+//    return new InMemoryUserDetailsManager(user1);
+//
+//  }
+
 }
