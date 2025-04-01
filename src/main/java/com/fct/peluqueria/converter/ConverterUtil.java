0147273f88e25b3b6b8957f1b1eb560e1f33a270
mirip1 <mivelfer5@gmail.com>
2025-04-01 20:31:14 +0200

    Reseñas implementadas

--- src/main/java/com/fct/peluqueria/converter/ConverterUtil.java
+++ src/main/java/com/fct/peluqueria/converter/ConverterUtil.java
@@ -6,8 +6,12 @@ import java.time.ZoneId;
 import com.fct.peluqueria.constants.Rol;
 import com.fct.peluqueria.dto.PeluqueriaDTO;
 import com.fct.peluqueria.dto.RegistroDTO;
+import com.fct.peluqueria.dto.ResenaDTO;
+import com.fct.peluqueria.dto.ServicioDTO;
 import com.fct.peluqueria.dto.UsuarioDTO;
 import com.fct.peluqueria.models.Peluqueria;
+import com.fct.peluqueria.models.Resena;
+import com.fct.peluqueria.models.Servicio;
 import com.fct.peluqueria.models.Usuario;
 
 /**
@@ -62,9 +66,54 @@ public class ConverterUtil {
         .descripcion(peluqueria.getDescripcion())
         .ubicacion(peluqueria.getUbicacion())
         .build();
+    
+  }
+  
+  /**
+   * Convierte de Servicio a PeluqueriaDTO
+   * @param servicio el Objeto a convertir
+   * @return ServicioDTO relleno
+   */
+  public static ServicioDTO servicioToServicioDTO(Servicio servicio) {
+    return ServicioDTO.builder()
+        .id(servicio.getId())
+        .nombre(servicio.getNombre())
+        .precio(servicio.getPrecio())
+        .build();
 
   }
   
   
+  /**
+   * Convierte de Resena a ResenaDTO
+   * @param resena el Objeto a convertir
+   * @return ResenaDTO relleno
+   */
+  public static ResenaDTO resenaToResenaDTO(Resena resena) {
+      return ResenaDTO.builder()
+          .id(resena.getId())
+          .usuarioId(resena.getUsuario().getId())
+          .comentario(resena.getComentario())
+          .puntuacion(resena.getPuntuacion())
+          .fecha(resena.getFecha())
+          .build();
+  }
+  
+  /**
+   * Convierte de Resena a ResenaDTO
+   * 
+   * @param resena el Objeto a convertir
+   * @return ResenaDTO relleno
+   */
+  public static Resena ResenaDTOToresena(ResenaDTO dto) {
+    LocalDateTime fechaActual = LocalDateTime.now(ZoneId.of("CET"));
+    Resena resena = Resena.builder()
+        .comentario(dto.getComentario())
+        .puntuacion(dto.getPuntuacion())
+        .fecha(fechaActual)
+        .build();
+
+    return resena;
+  }
 
 }
